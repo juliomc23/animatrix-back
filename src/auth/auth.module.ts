f333e85317a43bc8f686f8775445ecf5ee7ec6eb
juliomc23 <julito2302mc@gmail.com>
2024-01-09 14:56:43 +0100

    feat: added jwtService to auth service and auth module
    this change provides signup and login method jwtService sign method, to create access token and refresh token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,14 +9,21 @@ import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+
+export interface JwtTokenResponse {
+  accessToken: string;
+  refreshToken: string;
+}
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
-  async signup(createUserDto: CreateUserDto) {
+  async signup(createUserDto: CreateUserDto): Promise<JwtTokenResponse> {
     const { password, ...userData } = createUserDto;
     try {
       const encryptedPassword = this.encryptPassword(password);
@@ -28,15 +35,17 @@ export class AuthService {
 
       await this.userRepository.save(user);
 
-      return user;
+      const { accessToken, refreshToken } = this.createJwtToken(user.id);
+
+      return { accessToken, refreshToken };
     } catch (error) {
       this.handleDBError(error);
     }
   }
-  async login(loginUserDto: LoginUserDto) {
+  async login(loginUserDto: LoginUserDto): Promise<JwtTokenResponse> {
     const { email, password } = loginUserDto;
     const user = await this.userRepository.findOne({
-      select: { email: true, password: true },
+      select: { id: true, email: true, password: true },
       where: { email },
     });
 
@@ -50,15 +59,17 @@ export class AuthService {
     if (!isSamePassword)
       throw new UnauthorizedException('Invalid email or password');
 
-    return user;
+    const { accessToken, refreshToken } = this.createJwtToken(user.id);
+
+    return { accessToken, refreshToken };
   }
 
-  private handleDBError(error: any) {
+  private handleDBError(error: any): void {
     if (error.errno === 1062)
       throw new ConflictException('User already exists');
   }
 
-  private encryptPassword(password: string) {
+  private encryptPassword(password: string): string {
     return bcrypt.hashSync(
       password,
       +this.configService.getOrThrow('SALT_ROUNDS'),
@@ -68,7 +79,13 @@ export class AuthService {
   private comparePlainPasswordAndEncryptedPassword(
     plainTextPassword: string,
     encryptedPassword: string,
-  ) {
+  ): boolean {
     return bcrypt.compareSync(plainTextPassword, encryptedPassword);
+  }
+
+  private createJwtToken(id: string) {
+    const accessToken = this.jwtService.sign({ id });
+    const refreshToken = this.jwtService.sign({ id }, { expiresIn: '7d' });
+    return { accessToken, refreshToken };
   }
 }
